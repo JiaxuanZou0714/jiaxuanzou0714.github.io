1d1ba034ae001316e75bb9ec5c754a6e6cbcf0bf
JiaXuan Zou <3140143497@qq.com>
2026-03-13 16:47:21 +0800

    feat: Introduce utility styles for code, forms, progress bars, and other components, along with a search script.

--- _scripts/search.liquid.js
+++ _scripts/search.liquid.js
@@ -57,12 +57,25 @@ ninja.data = [
       {
         {%- assign title = post.title | escape | strip -%}
         id: "post-{{ title | slugify }}",
+        {%- assign title_html = title | truncatewords: 13 -%}
+        {%- if post.categories -%}
+          {%- for category in post.categories -%}
+            {%- assign category_escaped = category | escape -%}
+            {%- assign title_html = title_html | append: " <span class='ninja-badge ninja-category'>" | append: category_escaped | append: "</span>" -%}
+          {%- endfor -%}
+        {%- endif -%}
+        {%- if post.tags -%}
+          {%- for tag in post.tags -%}
+            {%- assign tag_escaped = tag | escape -%}
+            {%- assign title_html = title_html | append: " <span class='ninja-badge ninja-tag'>#" | append: tag_escaped | append: "</span>" -%}
+          {%- endfor -%}
+        {%- endif -%}
         {% if post.redirect == blank %}
-          title: "{{ title | truncatewords: 13 }}",
+          title: "{{ title_html }}",
         {% elsif post.redirect contains '://' %}
-          title: '{{ title | truncatewords: 13 }} <svg width="1.2rem" height="1.2rem" top=".5rem" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg"><path d="M17 13.5v6H5v-12h6m3-3h6v6m0-6-9 9" class="icon_svg-stroke" stroke="#999" stroke-width="1.5" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"></path></svg>',
+          title: '{{ title_html }} <svg width="1.2rem" height="1.2rem" top=".5rem" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg"><path d="M17 13.5v6H5v-12h6m3-3h6v6m0-6-9 9" class="icon_svg-stroke" stroke="#999" stroke-width="1.5" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"></path></svg>',
         {% else %}
-          title: "{{ title | truncatewords: 13 }}",
+          title: "{{ title_html }}",
         {% endif %}
         description: "{{ post.description | strip_html | strip_newlines | escape | strip }}",
         {%- assign post_aliases = "" -%}
